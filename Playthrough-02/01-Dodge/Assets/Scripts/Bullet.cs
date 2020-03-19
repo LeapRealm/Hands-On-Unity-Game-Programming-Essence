1c2532653b66cbc5c0a9cfa7b1635b235cbd5300
Jinwoo, Kim <admin@leaprealm.com>
2020-03-20 01:12:29 +0900

    2회차 닷지 프로젝트 추가

---
+++ Playthrough-02/01-Dodge/Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
+
+    private void Start() {
+        bulletRigidbody = GetComponent<Rigidbody>();
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        Destroy(gameObject, 3f);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            if(playerController != null) {
+                playerController.Die();
+            }
+        }
+    }
+}
---
+++ Playthrough-02/01-Dodge/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    public GameObject gameoverText;
+    public Text timeText;
+    public Text recordText;
+
+    private float surviveTime;
+    private bool isGameover;
+
+    private void Start() {
+        surviveTime = 0f;
+        isGameover = false;
+    }
+
+    private void Update() {
+        if(!isGameover) {
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time: " + (int)surviveTime;
+        } else {
+            if(Input.GetKeyDown(KeyCode.R)) {
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+
+    public void EndGame() {
+        isGameover = true;
+        gameoverText.SetActive(true);
+
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+        if(surviveTime > bestTime) {
+            bestTime = surviveTime;
+            PlayerPrefs.SetFloat("BestTIme", bestTime);
+        }
+        recordText.text = "Best TIme : " + (int)bestTime;
+    }
+}
